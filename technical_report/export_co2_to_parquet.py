7b5bdaa2569ac518fe87f7ed44a11d90a2d6cc56
Deputation <deputation2@gmail.com>
2026-08-01 13:05:27 +0200

    Cleaning

--- technical_report/export_co2_to_parquet.py
+++ technical_report/export_co2_to_parquet.py
@@ -4,8 +4,9 @@ from pyspark.sql import SparkSession
 PROCESSED_DATA_DIR = Path("../data")
 PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-CLEAN_DATA_DIR = Path("../data/cleaned")
-CLEAN_DATA_DIR.mkdir(parents=True, exist_ok=True)
+# create the directory even if this specific script does not use it
+CLEANED_DATA_DIR = Path("../data/cleaned")
+CLEANED_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 spark = SparkSession.builder \
     .appName("profiling") \
@@ -13,45 +14,13 @@ spark = SparkSession.builder \
     .config("spark.driver.memory", "16g") \
     .getOrCreate()
 
-cols = ["Country", "VFN", "Mh", "T", "Va", "Ve", "Mk", "Cn", "m (kg)", "Ewltp (g/km)", "Ft", "Fm", "ec (cm3)", "ep (KW)", "z (Wh/km)", "year", "r"]
-
-column_mapping = {
-    "Country": "member_state",
-    "VFN": "vehicle_family_id_number",
-    "Mh": "manufacturer_name_eu_standard_denomination",
-    "T": "type",
-    "Va": "variant",
-    "Ve": "version",
-    "Mk": "make",
-    "Cn": "commercial_name",
-    "m (kg)": "mass_in_running_order (kg)",
-    "Ewltp (g/km)": "co2_emissions_WLTP (g/km)",
-    "Ft": "fuel_type",
-    "Fm": "fuel_mode",
-    "ec (cm3)": "engine_capacity (cm3)",
-    "ep (KW)": "engine_power (KW)",
-    "z (Wh/km)": "electric_energy_consumption (Wh/km)",
-    "r": "registrations"
-}
-
 csv_name = "4_eea_co2_emissions_from_passenger_cars-001.csv"
 path = f"../data/{csv_name}"
 
+cols = ["Country", "VFN", "Mh", "T", "Va", "Ve", "Mk", "Cn", "m (kg)", "Ewltp (g/km)", "Ft", "Fm", "ec (cm3)", "ep (KW)", "z (Wh/km)", "year", "r"]
+
 df = spark.read.csv(path, header=True, inferSchema=True).select(*cols)
-
-for old_col, new_col in column_mapping.items():
-    df = df.withColumnRenamed(old_col, new_col)
-
-eu27_2020 = ['BE', 'BG', 'CZ', 'DK', 'DE', 'EE', 'IE', 'EL', 'ES', 'FR', 'HR', 'IT', 'CY', 'LV', 'LT', 'LU', 'HU', 'MT', 'NL', 'AT', 'PL', 'PT', 'RO', 'SI', 'SK', 'FI', 'SE']
-
-df_only_eu = df.filter(df["member_state"].isin(eu27_2020))
-
-df_only_eu.write.mode("overwrite") \
-    .option("compression", "gzip") \
-    .parquet(f"{CLEAN_DATA_DIR}/4c_eea_co2_emissions_from_passenger_cars-001.parquet")
 
 df.write.mode("overwrite") \
     .option("compression", "gzip") \
     .parquet(f"{PROCESSED_DATA_DIR}/4_eea_co2_emissions_from_passenger_cars-001.parquet")
-
-
